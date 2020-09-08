cd8b49283c3dd6843bd94cfda6465813b81add8e
powerworker1234@gmail.com <powerworker1234@gmail.com>
2020-09-08 14:02:11 +0800

    UI adjust

---
+++ pbltest/imglib.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+class Picture_part:
+    def __init__(self,filename):
+        self.filename = filename
+
+    def horizontal(self):
+        orginal_img = cv2.imdecode(np.fromfile(self.filename, np.uint8), cv2.IMREAD_COLOR)
+        h,w,c = orginal_img.shape
+        #橫式處理
+        if h<w :
+            new_w_start= int(w /30*16)
+            new_w_end = int(w /30*28)
+            new_h_start = int(h /21*4)
+            new_h_end = int(h /21*16)
+            img = orginal_img[new_h_start:new_h_end,new_w_start:new_w_end ]
+            return img
+        else :
+            return orginal_img
+
+    def vertical(self):
+        orginal_img = cv2.imdecode(np.fromfile(self.filename, np.uint8), cv2.IMREAD_COLOR)
+        h, w, c = orginal_img.shape
+        # 直式處理
+        if w < h:
+            new_w_start = int(w / 21 * 1.4)
+            new_w_end = int(w / 21 * 19.7)
+            new_h_start = int(h / 29.7 * 4.7)
+            new_h_end = int(h / 29.7 * 16.4)
+            # new_w_start = int(w / 21 * 4)
+            # new_w_end = int(w / 21 * 20)
+            # new_h_start = int(h / 30 * 5)
+            # new_h_end = int(h / 30 * 16)
+            img = orginal_img[new_h_start:new_h_end, new_w_start:new_w_end]
+            return img
+        else:
+            return orginal_img
---
+++ pbltest/views.py
@@ -0,0 +1,289 @@
+import os
+import time
+
+import cv2
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, reverse
+from django.template.loader import get_template
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+from login.models import CreateClass, CreateActivate, Group, User
+from .forms import CardForm, CreateCardForm, RowCreateCardForm
+from .imglib import Picture_part
+from .models import UPCard, Card, TestCard, RowCard
+
+#Todo
+def post_card(request, pk):
+    post = UPCard.objects.all()
+    card = UPCard.objects.get(id=pk)
+    groups = Group.objects.get(group_user=request.session['user_id'])
+    user = User.objects.filter(id=request.session['user_id']).values_list()[0]
+    author = request.session['user_id']
+    group = groups.group
+    activate = groups.activate
+    Card.group = groups
+    Card.activate = activate
+    Card.author = author
+      # groups = Group.objects.all();
+    if request.method == "POST":
+        card_form = CreateCardForm(request.POST)
+        if card_form.is_valid():
+            final_card = card_form.save()
+            url = reverse('preview_card', args=[final_card.pk])
+            return redirect(url)
+    else:
+        card_form = CreateCardForm()
+    context = {
+        'form': CreateCardForm(initial={'author': user[1]
+              ,'group':groups
+            , 'activate': activate}),
+        'pk': pk,
+        'card': card,
+        'user':user[1]
+         # 'groups': groups,
+    }
+    return render(request, "create_card.html", context)
+
+
+def post_card1(request, pk):
+    post = UPCard.objects.all()
+    card = UPCard.objects.get(id=pk)
+
+    if request.method == "POST":
+        card_form = RowCreateCardForm(request.POST)
+        if card_form.is_valid():
+            final_card = card_form.save()
+            url = reverse('preview_card1', args=[final_card.pk])
+            return redirect(url)
+    else:
+        card_form = RowCreateCardForm()
+    context = {
+        'form': card_form,
+        'pk': pk,
+        'card': card,
+    }
+    return render(request, "create_card_row.html", context)
+
+
+def check_create_card(request):
+    cards = Card.objects.all()
+    return render(request, 'create_card.html', {
+        'cards': cards
+    })
+
+
+def index(request):
+    create_class = CreateClass.objects.all()
+    context = {'create_class': create_class}
+    return render(request, 'index.html', context)
+
+
+def cardind(request, pk):
+    group = Group.objects.get(id=pk)
+    group_user = group.group_user.filter(id=request.session['user_id'])
+    up_cards = UPCard.objects.all()
+    context = {
+        'group': group,
+        'group_user': group_user,
+        'up_cards': up_cards,
+        'pk': pk
+    }
+    return render(request, 'card_base.html', context)
+
+#
+# def cardind(request, pk):
+#     group = Group.objects.get(id=pk)
+#     group_user = group.group_user.all()
+#     up_cards = UPCard.objects.all()
+#     context = {
+#         'group': group,
+#         'group_user': group_user,
+#         'up_cards': up_cards,
+#         'pk': pk
+#     }
+#     return render(request, 'card_base.html', context)
+
+#Todo cardmanage 轉群組需額外處理
+
+def card_manage(request):
+    up_cards = TestCard.objects.all().order_by('-groups')
+    groups = Group.objects.get(group_user=request.session['user_id'])
+    group_id = groups.id
+    return render(request, 'card_manage.html', {
+        'up_cards': up_cards,
+        'group_id': group_id,
+        'groups': groups
+
+    })
+
+
+def change_card(request, pk):
+    unit = Card.objects.get(id=pk)
+    card_form = CreateCardForm(request.POST or None, instance=unit)
+    context = {
+        'card_form': card_form,
+        'unit': unit,
+    }
+
+
+    if card_form.is_valid():
+        card_form.save()
+        return redirect('index')
+    return render(request, 'change_card.html', context)
+
+
+def change_card1(request, pk):
+    unit = RowCard.objects.get(id=pk)
+    card_form = RowCreateCardForm(request.POST or None, instance=unit)
+    context = {
+        'card_form': card_form,
+        'unit': unit,
+    }
+    if card_form.is_valid():
+        card_form.save()
+        return redirect('index')
+    return render(request, 'change_card1.html', context)
+
+
+# 20200728
+# 原urls.py 中 #path('upload_card/', UploadView.as_view(), name='upload_card'),
+# 改為    path('upload_card/', views.uploadcard(), name='upload_card'),
+def uploadcard(request):
+    cards: UPCard
+    if request.method == 'POST':
+        form = CardForm(request.POST, request.FILES)
+        if form.is_valid():
+            cards = form.save(commit=False) #UPCard 的實體
+            file = cards.cover.file.name
+            filename = os.path.basename(file)
+            #pic = Picture_part(filename)
+            pic = Picture_part('media/card/covers/'+filename)
+            cd = form.cleaned_data
+            if cards.class_material == '1': # 直式
+                cards.thumbnail = 'media/card/vertical/'+filename
+                cards.author_id = cd['author']
+                cards.activate_id = cd['activate']
+                cards.group_id = cd['group']
+                cards.save()
+                img = pic.vertical()
+                cv2.imencode('.JPG',img)[1].tofile(cards.thumbnail)
+            elif cards.class_material == '2': # 橫式
+                cards.thumbnail = 'media/card/horizontal/' + filename
+                cards.save()
+                img = pic.horizontal()
+                cv2.imencode('.JPG', img)[1].tofile(cards.thumbnail)
+                #cv2.imwrite( )
+            return redirect(reverse('check_card'))
+    else:
+        form = CardForm()
+        user_id = request.session['user_id']
+        user = User.objects.get(id=user_id)
+        UPCard.author = user.c_name
+        groups_name =  user.user_user.all().values_list('id','group','activate_id')[0]
+        UPCard.group = list(groups_name)[1]
+        activate_name = CreateActivate.objects.get(id=list(groups_name)[2])
+        UPCard.activate = activate_name
+
+        context = {'form':CardForm(initial={'author': request.session['user_id']
+              ,'group':list(groups_name)[0]
+            , 'activate': list(groups_name)[2]})
+            , 'user':user.c_name
+            , 'activate_name':activate_name
+            ,'groups_name': list(groups_name)[1]
+            , 'activate_name': activate_name
+
+                   }
+    return render(request, 'cardlist.html', context)
+
+
+def card_list(request):
+    if request.method == 'POST':
+        form = CardForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('check_card')
+    else:
+        form = CardForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'cardlist.html', context)
+
+
+def check_card(request):
+
+    up_cards = Card.objects.all()
+    carda = UPCard.objects.all()
+    context = {
+        'carda': carda,
+        'up_cards': up_cards
+    }
+    return render(request, 'check_card.html', context)
+
+
+def card_list_view(request):
+    up_cards = UPCard.objects.all()
+
+    user_id = request.session['user_id']
+    group = Group.objects.all()
+    context = {
+        'up_cards': up_cards,
+        'group': group
+    }
+    return render(request, "class_check_card.html", context)
+
+
+class UploadView(CreateView):
+    model = UPCard
+    form_class = CardForm
+    success_url = reverse_lazy("check_card")
+    template_name = "cardlist.html"
+
+
+def delete_card(request, pk):
+    pk = int(pk)
+    if request.method == "POST":
+        card = UPCard.objects.get(pk=pk)
+        card.delete()
+    return redirect('check_card')
+
+
+def screen_shot(request):
+    return render(request, 'testscreen.html')
+
+
+def preview_card(request, pk):
+    unit = Card.objects.get(id=pk)
+    id = unit.author
+    user = User.objects.filter(id=id).values()[0]
+    global bools
+    bools = False
+    context = {
+        'unit': unit,
+        'user': user
+    }
+    test = TestCard()
+    if request.method == "POST":
+        test.base_img = request.POST.get('result1')
+        test.base_card_id = unit.pk
+        test.groups_id = unit.group.id
+        test.save()
+        bools = True
+        if bools:
+            return redirect('card_manage')
+    return render(request,  'Card/final_card.html', context)
+
+
+def preview_card1(request, pk):
+    unit = RowCard.objects.get(id=pk)
+    context = {
+        'unit': unit
+    }
+    test = TestCard()
+    if request.method == "POST":
+        test.base_img = request.POST.get('result1')
+        test.base_card1_id = unit.pk
+        test.groups_id = unit.group.id
+        test.save()
+    return render(request,  'final_card1.html', context)
